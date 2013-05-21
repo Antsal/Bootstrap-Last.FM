a754218ea82923536c903d32dab21ff4d3da21a5
P. Colin Manikoth <pmanikoth@ewu.edu>
2013-05-21 12:56:26 -0700

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  
+  // Good stuff
+	$('#lastfm').lastFM({
+		username: 'ewuweblab',
+		apikey: 'bdafa5aeb85d3c787dd6c9c2f91fc872',
+		number: 3,
+		artSize: 'large',
+		onComplete: function(){
+			//Done
+
+		}
+	});
+
+
+});
